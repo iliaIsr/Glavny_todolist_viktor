0370cd21d662ccd6fceffba2cb63f36f77aa83ab
iliaIsr <ilil.sbl91@gmail.com>
2024-12-29 16:26:07 +0300

    16 less 3.45 render

--- src/app/Main.tsx
+++ src/app/Main.tsx
@@ -1,17 +1,26 @@
 import Container from "@mui/material/Container"
-import React from "react"
+import React, { useEffect } from "react"
 import { AddItemForm } from "common/components"
-import { useAppDispatch } from "common/hooks"
+import { useAppDispatch, useAppSelector } from "common/hooks"
 import { addTodolistTC } from "../features/todolists/model/todolists-reducer"
 import { Todolists } from "../features/todolists/ui/Todolists/Todolists"
 import { Grid } from "@mui/material"
+import { useNavigate } from "react-router"
+import { Path } from "common/routing/Routing"
 
 export const Main = () => {
   const dispatch = useAppDispatch()
+  const isLoggedIn = useAppSelector((state) => state.auth.isLoggedIn)
+  const navigate = useNavigate()
 
   const addTodolist = (title: string) => {
     dispatch(addTodolistTC(title))
   }
+  useEffect(() => {
+    if (!isLoggedIn) {
+      navigate(Path.Login)
+    }
+  }, [isLoggedIn])
 
   return (
     <Container fixed>

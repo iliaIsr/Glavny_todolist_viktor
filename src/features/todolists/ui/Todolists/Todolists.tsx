02d8ce3c82c218c7eca2980794224a0ad214d4b3
iliaIsr <ilil.sbl91@gmail.com>
2025-01-21 10:14:37 +0300

    19 withOut Error's

--- src/features/todolists/ui/Todolists/Todolists.tsx
+++ src/features/todolists/ui/Todolists/Todolists.tsx
@@ -3,10 +3,23 @@ import Paper from "@mui/material/Paper"
 import { useGetTodolistsQuery } from "../../api/todolistsApi"
 import { Todolist } from "./Todolist/Todolist"
 import Grid from "@mui/material/Grid"
+import { Skeleton } from "@mui/material"
+import { TodolistSkeleton } from "../skeletons/TodolistSkeleton/TodolistSkeleton"
 
 export const Todolists = () => {
-  const { data: todolists } = useGetTodolistsQuery()
+  const { data, data: todolists, isLoading, isFetching } = useGetTodolistsQuery()
 
+  if (isLoading) {
+    return (
+      <div style={{ display: "flex", justifyContent: "space-between", gap: "32px" }}>
+        {Array(3)
+          .fill(null)
+          .map((_, id) => (
+            <TodolistSkeleton key={id} />
+          ))}
+      </div>
+    )
+  }
   return (
     <>
       {todolists?.map((tl) => {
